85fc9309edefd468f28cc28ed146388016fd4504
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-02-11 20:18:07 -0600

    added user model

--- models/users.js
+++ models/users.js
@@ -1,14 +1,43 @@
-//username
-//String, Unique, Required, Trimmed
+const { Schema, model } = require('mongoose');
 
-//email
-//String, Required, Unique, valid email format
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Invalid Email Address']
+      },      
+      thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thoughtSchema'
+      }],
+      friends: [{
+          type: Schema.Types.ObjectId,
+          ref: 'userSchema'
+        }]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      // prevents virtuals from creating duplicate of _id as `id`
+      id: false
+    }
+  );
 
-//thoughts
-//array of _id referencing thoughts
-
-//friends
-//array of _id referencing user model (self-reference)
-
-//Virtual friendCount
-//count of friends
+  userSchema.virtual('friendsCount').get(function() {
+    return this.friends.length + 1
+    
+  });
+  
+  const User = model('User', userSchema);
+  
+  module.exports = Pizza;
